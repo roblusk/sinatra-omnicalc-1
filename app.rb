bebfb4ac70937960f45852ffa0c02278a845e552
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-12 04:16:54 +0000

    completed square form

--- app.rb
+++ app.rb
@@ -6,4 +6,18 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  erb(:home)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square_root/new") do
+end
+
+get("/payment/new") do
+end
+
+get("/random/new") do
 end
